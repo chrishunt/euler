52c466e8463aecc42ebeee9b680a0ee2cd44fe14
Chris Hunt <c@squareup.com>
2012-09-08 17:17:54 -0700

    Add solution to problem 002

---
+++ 002-sum-fibonacci/lib/fib.rb
@@ -0,0 +1,17 @@
+class Fib
+  def self.even_sum(n)
+    return 0 if n <= 2
+
+    previous_previous = 1
+    previous = 2
+    sum = 2
+
+    while (current = previous + previous_previous) < n
+      sum += current if current % 2 == 0
+      previous_previous = previous
+      previous = current
+    end
+
+    return sum
+  end
+end
