cb02706c9390523b76790bf9cce286b38b1cb756
Chris Hunt <c@squareup.com>
2013-01-17 13:51:08 -0800

    Use simple iterators for a bit more speed

--- 004-palindromic-number/lib/palindrome.rb
+++ 004-palindromic-number/lib/palindrome.rb
@@ -6,15 +6,22 @@ end
 
 class Palindrome
   def of_multiples(digits)
-    upper_limit = ('9' * digits).to_i
-    lower_limit = ('1' + ('0' * (digits - 1))).to_i
+    upper_factor = ('9' * digits).to_i
+    lower_factor = ('1' + ('0' * (digits - 1))).to_i
 
-    (upper_limit * upper_limit).downto(lower_limit * lower_limit).each do |i|
-      next unless i.to_s.is_palindrome?
+    upper_product = upper_factor * upper_factor
+    lower_product = lower_factor * lower_factor
 
-      upper_limit.downto(lower_limit).each do |j|
-        next unless (i % j == 0) && (i / j <= upper_limit)
-        return i
+    i = upper_product
+
+    while (i -= 1) > lower_product
+      if i.to_s.is_palindrome?
+        j = upper_factor
+        while (j -= 1) > lower_factor
+          if (i % j == 0) && (i / j <= upper_factor)
+            return i
+          end
+        end
       end
     end
   end
