9541aa9c854dfa909c90009bfc6bfb20f2d58473
Chris Hunt <c@squareup.com>
2013-01-17 12:56:06 -0800

    Don't check the same multiple twice

--- 004-palindromic-number/lib/palindrome.rb
+++ 004-palindromic-number/lib/palindrome.rb
@@ -7,11 +7,12 @@ end
 class Palindrome
   def of_multiples(digits)
     upper_limit = ('9' * digits).to_i
+    lower_limit = ('1' + ('0' * (digits - 1))).to_i
 
     largest_palindrome = 0
 
-    (0..upper_limit).each do |n|
-      (0..upper_limit).each do |m|
+    (lower_limit..upper_limit).each do |n|
+      (n..upper_limit).each do |m|
         result = n * m
 
         if (result > largest_palindrome) && result.to_s.is_palindrome?
