88ca7b7c45d9f2408d29aab484bba849d03ffcff
Chris Hunt <c@squareup.com>
2013-01-17 11:21:38 -0800

    Change #add, #fizz_buzzes to instance methods

--- 001-natural-numbers/lib/fizz_buzz.rb
+++ 001-natural-numbers/lib/fizz_buzz.rb
@@ -1,9 +1,9 @@
 class FizzBuzz
-  def self.add(range)
+  def add(range)
     fizz_buzzes(range).inject(0) { |sum, i| sum + i }
   end
 
-  def self.fizz_buzzes(range)
+  def fizz_buzzes(range)
     (1...range).map { |i| i if (i % 3 == 0 || i % 5 == 0) }.compact
   end
 end
--- 001-natural-numbers/spec/fizz_buzz_spec.rb
+++ 001-natural-numbers/spec/fizz_buzz_spec.rb
@@ -7,22 +7,22 @@ require 'fizz_buzz'
 describe FizzBuzz do
   describe '#add' do
     it 'works with 5' do
-      FizzBuzz.add(5).should == 3
+      subject.add(5).should == 3
     end
 
     it 'works with 10' do
-      FizzBuzz.add(10).should == 23
+      subject.add(10).should == 23
     end
 
     it 'finds the solution to the problem' do
-      FizzBuzz.add(1000).should == 233168
+      subject.add(1000).should == 233168
     end
   end
 
   describe '#fizz_buzzes' do
     it 'collects all multiples of 3 and 5' do
-      FizzBuzz.fizz_buzzes(10).should == [3, 5, 6, 9]
-      FizzBuzz.fizz_buzzes(12).should == [3, 5, 6, 9, 10]
+      subject.fizz_buzzes(10).should == [3, 5, 6, 9]
+      subject.fizz_buzzes(12).should == [3, 5, 6, 9, 10]
     end
   end
 end
